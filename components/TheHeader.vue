26b005ddf88796e3ff1927be226f318b612e244c
cosmob3 <bryanlvelasco@hotmail.com>
2022-04-20 11:30:23 -0600

    added more stylings

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="flex justify-center py-10 bg-violet-500 rounded-2xl m-16">
     <div class="bg-violet-500">
-      <NuxtLink class="px-10 bg-violet-500" to="/simple-formkit"
+      <NuxtLink
+        class="px-10 text-white hover:text-orange-500 bg-violet-500 text-3xl"
+        to="/simple-formkit"
         >Simple Form</NuxtLink
       >
     </div>
-    <h1 class="text-6xl text-zinc-100 bg-violet-500 text-center">
-      Welcome, stay a while...
-    </h1>
+    <a href="/">
+      <h1 class="text-6xl text-zinc-100 bg-violet-500 text-center">
+        Welcome, stay a while...
+      </h1>
+    </a>
     <div class="bg-violet-500">
-      <NuxtLink class="px-10 bg-violet-500" to="/contact">Contact</NuxtLink>
+      <NuxtLink
+        class="px-10 text-white hover:text-orange-500 bg-violet-500 text-3xl"
+        to="/contact"
+        >Contact</NuxtLink
+      >
     </div>
   </div>
 </template>
